def2d2f12e3b5cd3137684da53289e3d51ba9d81
Lou Duquenoy <72021807+Lou-Duquenoy@users.noreply.github.com>
2022-01-31 20:04:46 +0100

    Final-version

--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -7,6 +7,7 @@ router.get('/', (req, res) => {
         .then(movies => res.json(movies))
 })
 
+
 router.get('/:movieId',(req,res) => {
     sequelize.models.movie.findByPk(req.params.movieId)
         .then(movie => res.json(movie))
